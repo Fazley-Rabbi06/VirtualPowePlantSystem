66c6ec183cc2cf16bbb9e5aa642cad6a44670065
Fazley-Rabbi06 <fazleyrabbi563622@gmail.com>
2024-08-20 13:03:52 +0600

    implement validation implement test cases

--- src/main/java/com/powerplantsystem/virtualpowerplantsystem/controller/BatteryController.java
+++ src/main/java/com/powerplantsystem/virtualpowerplantsystem/controller/BatteryController.java
@@ -5,7 +5,11 @@ import com.powerplantsystem.virtualpowerplantsystem.dto.BatteryStatistics;
 import com.powerplantsystem.virtualpowerplantsystem.entity.Battery;
 import com.powerplantsystem.virtualpowerplantsystem.service.BatteryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+import com.powerplantsystem.virtualpowerplantsystem.validator.BatteryValidator;
 
 import java.util.Comparator;
 import java.util.List;
@@ -19,10 +23,28 @@ public class BatteryController {
     @Autowired
     private BatteryService batteryService;
 
+    @Autowired
+    private BatteryValidator batteryValidator;
+
     @PostMapping
-    public List<Battery> addBatteries(@RequestBody List<Battery> batteries) {
-        return batteryService.saveBatteries(batteries);
+    public ResponseEntity<?> addBatteries(@RequestBody List<Battery> batteries, BindingResult result) {
+        // Validate the list of batteries
+        batteryValidator.validate(batteries, result);
+
+        if (result.hasErrors()) {
+            // Collect all validation errors
+            List<String> errorMessages = result.getAllErrors()
+                    .stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .collect(Collectors.toList());
+            // Return bad request with error messages
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+
+        // Save the batteries and return the saved entities
+        return ResponseEntity.ok(batteryService.saveBatteries(batteries));
     }
+
 
     @GetMapping
     public BatteryResponse getBatteriesInRange(
